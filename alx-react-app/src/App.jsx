618d355a9fdcceae8a4b0d508bff46a7531eb8fa
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-08-06 06:46:03 +0300

    change

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import WelcomeMessage from './components/WelcomeMessage'
-
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer.'
 function App() {
 
   return (
-    <div> 
-      <WelcomeMessage/>
-    </div>
+     <div>
+       <h1>Hello everyone, I am learning React at ALX!</h1>
+       <p>This is a simple JSX component.</p>
+       <p>I am learning about JSX!</p>
+     </div>
+
+    
   )
 }
 
