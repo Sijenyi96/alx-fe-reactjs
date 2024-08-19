a669b50d3ab55ad9096a4f7b206d6528f740b7fe
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-08-20 00:47:44 +0300

    changes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
@@ -17,23 +16,4 @@ const RecipeList = () => {
 };
 
 export default RecipeList;
-=======
-import { useRecipeStore } from './recipeStore';
 
-const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
-
-  return (
-    <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default RecipeList;
->>>>>>> b43c2a73c822c72517bca51b39da8fd6de08c7ee
