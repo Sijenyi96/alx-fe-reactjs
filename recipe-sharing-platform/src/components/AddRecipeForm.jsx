92173f66cc09530c13accd175bdb2c968d8bcf35
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-09-13 11:24:06 +0300

    add changes

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -1,18 +1,33 @@
 import { useState, useEffect } from "react";
 import data from "../data.json";
 
-const HomePage = () => {
-  const [recipes, setRecipes] = useState([]);
+const AddRecipeForm = () => {
+  const [title, setTitle] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [steps, setSteps] = useState('');
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    setRecipes(data);
+    setTitle('');
+    setIngredients('');
+    setSteps('');
+    setErrors('');
   }, []);}
 
 
 
 function AddRecipeForm() {
+
+    const handleSubmit = (event) => {
+        const value = event.target.value;
+        setTitle(values => ({...values, ['']: value}))
+        setIngredients(values => ({...values, ['']: value}))
+        setSteps(values => ({...values, ['']: value}))
+        setErrors(values => ({...values, ['']: value}))
+      }
+
     return ( 
-        <form>
+        <form onSubmit={handleSubmit} >
            <label>Recipe title:
                 <input type="text" />
            </label>
