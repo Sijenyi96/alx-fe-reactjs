2d495327131c76bc8ca3fd45f07308a1cae9469f
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-09-19 00:14:36 +0300

    add changes

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,12 +1,28 @@
 import React, {useState} from 'react';
-
+import fetchUserData from '../services/githubService';
 
 const Search =() => {
   const [username, setUsername] =useState ('');
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [error, setError] =useState ('');
-  const handleSubmit = (e) =>{
+  const handleSubmit = async(e) =>{
     e.preventDefault();
-}
+    setLoading(true);
+    setError(null);
+    setUserData(null);
+
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data);
+    } catch (error) {
+      setError('Looks like we can’t find the user');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
 
 [ "if (! username )", setError]
 
