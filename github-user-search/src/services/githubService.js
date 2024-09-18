370a8fa30dd3c02068edf42f4d58f14bbf6470fe
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-09-19 02:28:43 +0300

    add changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -18,7 +18,7 @@ const buildQuery = ({ username, location, minRepos }) => {
 const fetchUserData = async ({username, location, minRepos}) => {
     const query = buildQuery({ username, location, minRepos});
     try {
-    const response = await axios.get(`https://api.github.com/users?q={query}`);
+    const response = await axios.get(`https://api.github.com/users?q`);
     return response.data.items;
     } catch(error) {
         console.error('Error fetching GitHub users:',error);
